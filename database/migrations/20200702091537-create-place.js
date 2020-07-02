363064500ed62c7ce3d8a6c7bbadc26a8b996cac
Stéphane PATERNA <paterna@club-internet.fr>
2020-07-02 11:43:50 +0200

    Création des tables Place et City

---
+++ database/migrations/20200702091537-create-place.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Places', {
+      id_place: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      rooms: {
+        type: Sequelize.INTEGER,
+      },
+      bathrooms: {
+        type: Sequelize.INTEGER,
+      },
+      max_guests: {
+        type: Sequelize.INTEGER,
+      },
+      price_by_night: {
+        type: Sequelize.FLOAT,
+      },
+      city_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Cities',
+          key: 'id',
+        },
+      },
+      host_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Places');
+  },
+};
